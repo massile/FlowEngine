a8e5fff4c666cc1824b83ca1924492d3febfad73
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2016-10-25 22:41:39 +0200

    Add dependencies to OpenGL GLEW and GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
+    glfwInit();
+    glewInit();
+    glClearColor(0, 0, 0, 0);
     std::cout << "test";
  	return 0;
 }
