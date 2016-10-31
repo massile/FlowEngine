4d3ad439f3e5e19c3a36fe8c7da0f5968b539ba4
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2016-10-31 05:03:44 +0100

    Create World service to store common data

---
+++ src/services/environment/Environment.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tessellator on 30/10/16.
+//
+
+#include "Environment.h"
---
+++ src/services/environment/world/IWorld.h
@@ -0,0 +1,8 @@
+//
+// Created by tessellator on 30/10/16.
+//
+
+#ifndef FLOWENGINE_IWORLD_H
+#define FLOWENGINE_IWORLD_H
+
+#endif //FLOWENGINE_IWORLD_H
